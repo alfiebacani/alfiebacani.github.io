b83d2b4096e1373564964a19070371ab46fb8be2
Suitestudio <suitestudio@SuitestarksMini.localdomain>
2019-04-03 14:46:48 +0800

    Update

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="Travinasia Website is coming soon">
+  <meta name="author" content="Web Developer: Alfie Mojar Bacani, bacani_fie04@yahoo.com">
+
+  <title>Travinasia Website is coming soon</title>
+
+  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css?family=Questrial|Raleway" rel="stylesheet">
+
+
+</head>
+
+<body>
+
+<h1 style="font-family:Raleway;text-align: center;">Travinasia Website is Coming Soon!</h1>
+
+</body>
+
+</html>
